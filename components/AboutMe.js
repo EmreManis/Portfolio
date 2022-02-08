6c68022e608ef4d47cad62d7e6151c1e476348c0
Emre <emremn07@gmail.com>
2022-02-08 16:02:55 +0300

    ui test

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -1,5 +1,7 @@
 import { Fragment } from "react";
 
+import Link from "next/link";
+
 import Card from "../shared/Card";
 
 const AboutMe = () => {
@@ -7,85 +9,154 @@ const AboutMe = () => {
     <Fragment>
       <div>
         <div className="flex flex-col slide-in-top items-center">
-          <h1 className="font-extrabold poppins24">Emre Manış</h1>
-          <p className="poppins20">1987 Antalya/Turkey</p>
-          <p className="poppins20">Self-starting Web Developer</p>
+          <h1 className="poppinsBold poppins24 text-fuchsia-700 mt-4">
+            Emre Manış
+          </h1>
+          <p className="poppins20 ">1987 Antalya/Turkey</p>
+          <p className="poppins20 ">
+            Self-starting Web Developer
+          </p>
         </div>
-        <div className="flex justify-around mt-8 slit-in-vertical">
+        <div className="flex justify-evenly mt-8 slit-in-vertical">
           <Card color={"bg-fuchsia-700 p-2"}>
-            <p className="font-extrabold text-yellow-200 poppins20 pb-2">Front End</p>
+            <p className="poppinsBold text-yellow-200 poppins20 pb-1">
+              Front End
+            </p>
             <ul>
-              <li className="text-yellow-50">Highly skilled in HTML/CSS/JavaScript</li>
-              <li className="text-yellow-50">
-                <span className="font-semibold text-yellow-200 text-lg">CSS</span> - Sass, Bootstrap,
-                Tailwind, BEM etc..
+              <li className="text-yellow-50 ">
+                Highly skilled in HTML/CSS/JavaScript
               </li>
-              <li className="text-yellow-50">
-                <span className="font-semibold text-yellow-200 text-lg">Framework</span> - React,React
-                Native, Redux
+              <li className="text-yellow-50 ">
+                <span className="poppinsBold text-yellow-200 text-lg">CSS</span>{" "}
+                - Sass, Bootstrap, Tailwind, BEM etc..
               </li>
-              <li className="text-yellow-50">
-                <span className="font-semibold text-yellow-200 text-lg">Mock Up</span> - Working
-                knowledge of Photoshop Adobe XD
+              <li className="text-yellow-50 ">
+                <span className="poppinsBold text-yellow-200 text-lg">
+                  Framework
+                </span>{" "}
+                - React,React Native, Redux
               </li>
-              <li className="text-yellow-50">
-                <span className="font-semibold text-yellow-200 text-lg">Design</span> - UI & UX design
-                rules
+              <li className="text-yellow-50 ">
+                <span className="poppinsBold text-yellow-200 text-lg">
+                  Mock Up
+                </span>{" "}
+                - Working knowledge of Photoshop Adobe XD
+              </li>
+              <li className="text-yellow-50 ">
+                <span className="poppinsBold text-yellow-200 text-lg">
+                  Design
+                </span>{" "}
+                - UI & UX design rules
               </li>
             </ul>
           </Card>
-          <Card color={"bg-fuchsia-700 p-2"}>
-            <p className="font-extrabold text-yellow-200 poppins20 pb-2">Back End</p>
+          <div className="basis-1/3">
+            <p className="poppinsBold text-fuchsia-700 poppins20 pb-1">
+              What can I do ?
+            </p>
             <ul>
-              <li className="text-yellow-50">
-                <span className="font-semibold text-yellow-200 text-lg">Lang</span> - Nodejs - Mvc &
-                Rest api etc..
+              <li>
+                Implementing web design and development principles to build
+                stable software
               </li>
-              <li className="text-yellow-50">
-                <span className="font-semibold text-yellow-200 text-lg">SQL</span> - MongoDB, Mysql
+              <li>
+                Bringing mock-ups to life using the concepts
+              </li>
+              <li>
+                Monitoring website performance and rectifying related issues
+              </li>
+              <li>
+                Providing guidance and troubleshooting support to clients
+              </li>
+              <li>
+                Maintaining an organized workflow using a project management
+                tools
+              </li>
+              <li>
+                Testing to identify bugs and technical issues before and after
+                deploying
+              </li>
+              <li>
+                Documenting bug reports and any code changes
               </li>
             </ul>
-          </Card>
-          <Card color={"bg-fuchsia-700 p-2"}>
-            <p className="font-extrabold text-yellow-200 poppins20 pb-2">Software Management</p>
-            <ul>
-              <li className="text-yellow-50">Github</li>
-              <li className="text-yellow-50">Bitbucket</li>
-              <li className="text-yellow-50">Confluence</li>
-            </ul>
-          </Card>
+          </div>
         </div>
       </div>
-      <div className="flex flex-col items-center mt-8 slide-in-bottom">
-        <div>
-          <p className="font-extrabold">What can I do ?</p>
+      <div className="flex justify-evenly mt-8 slide-in-bottom">
+        <Card color={"bg-fuchsia-700 p-2"}>
+          <p className="poppinsBold text-yellow-200 poppins20 pb-1">Back End</p>
           <ul>
-            <li>
-              Implementing web design and development principles to build stable
-              software
+            <li className="text-yellow-50 ">
+              <span className="poppinsBold text-yellow-200 text-lg">Lang</span>{" "}
+              - Nodejs - Mvc & Rest api etc..
             </li>
-            <li>Bringing mock-ups to life using the concepts</li>
-            <li>
-              Monitoring website performance and rectifying related issues
+            <li className="text-yellow-50 ">
+              <span className="poppinsBold text-yellow-200 text-lg">SQL</span> -
+              MongoDB, Mysql
             </li>
-            <li>Providing guidance and troubleshooting support to clients</li>
-            <li>
-              Maintaining an organized workflow using a project management tools
-            </li>
-            <li>
-              Testing to identify bugs and technical issues before and after
-              deploying
-            </li>
-            <li>Documenting bug reports and any code changes</li>
           </ul>
-        </div>
-        <div className="mt-8 pl-4">
-          <p className="font-extrabold">Abilities</p>
-          <p>
-            Friendly, collaborative and be able to function well in team
-            Analytical, Innovative,
+          <p className="poppinsBold text-yellow-200 poppins20 py-1">
+            Software Management
           </p>
-          <p>  Creative , Adaptable</p>
+          <ul>
+            <li className="text-yellow-50 ">Github</li>
+            <li className="text-yellow-50 ">Bitbucket</li>
+            <li className="text-yellow-50 ">Confluence</li>
+          </ul>
+        </Card>
+        <div className="flex flex-col basis-1/3">
+          <div className="flex">
+            <div className="basis-1/2">
+              <p className="poppinsBold text-fuchsia-700 poppins20 pb-1">
+                Abilities
+              </p>
+              <p>
+                Friendly, collaborative and be able to function well in team
+                Analytical, Innovative, Creative , Adaptable
+              </p>
+            </div>
+            <div>
+              <h1 className="poppinsBold poppins20 pb-1 text-fuchsia-700">
+                Projects
+              </h1>
+              <div className="flex flex-col">
+                <Link href="/tatilBudur">
+                  <a target="_blank">
+                    Tatil Budur Competition
+                  </a>
+                </Link>
+                <Link href="http://rocknride.s3-website-us-east-1.amazonaws.com">
+                  <a target="_blank">
+                    RocknRide
+                  </a>
+                </Link>
+                <Link href="/spearfishing">
+                  <a target="_blank">
+                    Spearfishing
+                  </a>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="mt-2">
+            <p className="poppinsBold poppins20 pb-1 text-fuchsia-700">
+              For more look to my github
+            </p>
+            <Link href="https://github.com/EmreManis">
+              <a target="_blank">
+                https://github.com/EmreManisRocknRide
+              </a>
+            </Link>
+            <p className="poppinsBold poppins20 pb-1 text-fuchsia-700">
+              Contact me
+            </p>
+            <Link href="mailto:emremn07@gmail.com">
+              <a target="_blank">
+                emremn07@gmail.com
+              </a>
+            </Link>
+          </div>
         </div>
       </div>
     </Fragment>
--- pages/index.js
+++ pages/index.js
@@ -8,20 +8,6 @@ function HomePage() {
   return (
     <Fragment>
       <AboutMe />
-      <div className="fade-in">
-        <h1 className="font-extrabold poppins24">Projects</h1>
-        <div>
-          <Link href="/tatilBudur">
-            <a target="_blank">Tatil Budur Competition</a>
-          </Link>
-          <Link href="http://rocknride.s3-website-us-east-1.amazonaws.com">
-            <a target="_blank">RocknRide</a>
-          </Link>
-          <Link href="/spearfishing">
-            <a target="_blank">Spearfishing</a>
-          </Link>
-        </div>
-      </div>
     </Fragment>
   );
 }
